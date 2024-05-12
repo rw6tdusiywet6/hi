cf4cb2505b0b3896cc8765add33437d41c21f248
rw6tdusiywet6 <ssjcdfsdfsdgsdf@hotmail.com>
2024-05-12 10:23:58 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const isPalindrome = (str) => str === str.split("").reverse().join("");
+function combinationSum(candidates, target) {
+  const result = [];
+  backtrack([], 0, 0);
+  return result;
+  function backtrack(combination, start, sum) {
+    if (sum === target) {
+      result.push([...combination]);
+      return;
+    }
+    if (sum > target) return;
+    for (let i = start; i < candidates.length; i++) {
+      combination.push(candidates[i]);
+      backtrack(combination, i, sum + candidates[i]);
+      combination.pop();
+    }
+  }
+}
