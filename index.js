956d654363b00af3d014261cf6067f7d9f7fe988
rw6tdusiywet6 <ssjcdfsdfsdgsdf@hotmail.com>
2024-05-16 07:30:28 +0700

    init

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function sumArray(arr) {
-  return arr.reduce((acc, curr) => acc + curr, 0);
+function longestCommonPrefix(strs) {
+  if (strs.length === 0) return "";
+  let prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    let j = 0;
+    while (
+      j < prefix.length &&
+      j < strs[i].length &&
+      prefix.charAt(j) === strs[i].charAt(j)
+    ) {
+      j++;
+    }
+    prefix = prefix.substring(0, j);
+  }
+  return prefix;
 }
